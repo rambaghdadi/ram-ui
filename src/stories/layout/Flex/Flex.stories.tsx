c49ae5f9243be367b171ea1473672dff5241ddf2
Ram Baghdadi <ram_baghdadi@hotmail.com>
2023-08-01 22:53:02 +0100

    added story for flex and updated button text to center

---
+++ src/stories/layout/Flex/Flex.stories.tsx
@@ -0,0 +1,45 @@
+import type {Meta, StoryObj} from "@storybook/react"
+
+import Flex from "./Flex"
+import Button from "../../atoms/Button/Button"
+
+const meta = {
+  title: "LAYOUT/Flex",
+  component: Flex,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Flex>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const Template: Story = {
+  render: (args) => {
+    return (
+      <Flex {...args}>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </Flex>
+    )
+  },
+}
+
+export const Column: Story = {
+  ...Template,
+  args: {
+    alignItems: "stretch",
+    gap: 1,
+    flexDirection: "column",
+  },
+}
+export const Row: Story = {
+  ...Template,
+  args: {
+    alignItems: "stretch",
+    gap: 1,
+    flexDirection: "row",
+  },
+}
