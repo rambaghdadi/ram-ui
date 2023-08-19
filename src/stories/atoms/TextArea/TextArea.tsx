3488effce7b5a2836a2dc826f926962f4a6f15cd
Ram Baghdadi <ram_baghdadi@hotmail.com>
2023-08-19 20:43:12 +0100

    Updated text area component to update height based on scroll height

--- src/stories/atoms/TextArea/TextArea.tsx
+++ src/stories/atoms/TextArea/TextArea.tsx
@@ -1,3 +1,4 @@
+import {useRef} from "react"
 import classes from "./TextArea.module.css"
 import {ITextAreaProps} from "./TextArea.types"
 
@@ -12,8 +13,18 @@ export default function TextArea({
   id,
   ...props
 }: ITextAreaProps) {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
   const errorCls = !!error ? classes.error : ""
   const disabledCls = disabled ? classes.disabled : ""
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const textArea = textAreaRef.current
+    if (textArea) {
+      textArea.style.height = "auto"
+      textArea.style.height = `${textArea.scrollHeight}px`
+    }
+    onChange(e)
+  }
 
   return (
     <>
@@ -22,10 +33,12 @@ export default function TextArea({
           {placeholder}
         </label>
         <textarea
+          ref={textAreaRef}
           aria-invalid={!!error}
           placeholder=" "
+          onChange={onChangeHandler}
           className={`${classes.input} ${errorCls} ${disabledCls}`}
-          {...{...props, value, onChange, required, disabled, name, id}}
+          {...{...props, value, required, disabled, name, id}}
         />
         <p className={`${classes.placeholder} ${errorCls}`}>
           {(error && "Invalid Input") ||
